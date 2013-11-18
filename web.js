5d7eebbcaffeafe4ed99450177f70e2c64de7b54
David Gailey <dave.g.github@gmail.com>
2013-11-18 11:45:51 -0500

    modified web.js to deliver static content and index.html if at the root domain

--- web.js
+++ web.js
@@ -1,12 +1,30 @@
 var express = require('express');
+var fs = require('fs');
+
 var app = express();
 app.use(express.logger());
+app.use(express.compress());
+app.use('/javascripts',express.static(__dirname + '/javascripts'));
+app.use('/stylesheets',express.static(__dirname + '/stylesheets'));
+
+/*
+app.configure(function(){
+  app.use('/media', express.static(__dirname + '/media'));
+  app.use(express.static(__dirname + '/public'));
+});
+*/
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	var indexContents = fs.readFile('index.html', function (err, data) {
+		if (err) throw err;
+		console.log(data);
+		response.send(data.toString());
+	});
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
