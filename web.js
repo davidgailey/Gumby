494a5b18bdf0d5f4c34cabf1d08d9ed086ef49bd
David Gailey <dave.g.github@gmail.com>
2013-11-18 14:18:16 -0500

    install of node modules, modify web.js to serve static files

--- web.js
+++ web.js
@@ -1,30 +1,28 @@
-var express = require('express');
-var fs = require('fs');
+var express = require('express'),
+	fs = require('fs'),
+	port = process.env.PORT || 5000;
 
 var app = express();
-app.use(express.logger());
-app.use(express.compress());
-app.use('/javascripts',express.static(__dirname + '/javascripts'));
-app.use('/stylesheets',express.static(__dirname + '/stylesheets'));
-
-/*
 app.configure(function(){
-  app.use('/media', express.static(__dirname + '/media'));
-  app.use(express.static(__dirname + '/public'));
+	app.use(express.logger());
+	app.use(express.compress());
+	app.use(express.methodOverride());
+	app.use(express.bodyParser());
+	app.use(express.static(__dirname));
+	app.use(express.errorHandler({
+		dumpExceptions: true, 
+		showStack: true
+	}));
+	app.use(app.router);
 });
-*/
-
 
 app.get('/', function(request, response) {
 	var indexContents = fs.readFile('index.html', function (err, data) {
 		if (err) throw err;
-		console.log(data);
 		response.send(data.toString());
 	});
 });
 
-
-var port = process.env.PORT || 5000;
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
